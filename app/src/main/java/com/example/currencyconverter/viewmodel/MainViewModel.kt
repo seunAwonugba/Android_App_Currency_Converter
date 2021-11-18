66227ab6e93ab041d5665ba2b4d3dd817d0d4f9b
seunawonugba <seun.awonugba@decagon.dev>
2021-11-19 00:27:28 +0100

    implement convertion logic in view model

--- app/src/main/java/com/example/currencyconverter/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/currencyconverter/viewmodel/MainViewModel.kt
@@ -1,12 +1,17 @@
 package com.example.currencyconverter.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.currencyconverter.dataclass.Rates
 import com.example.currencyconverter.repository.TestRepository
+import com.example.currencyconverter.utils.ApiCallErrorHandler
 import com.example.currencyconverter.utils.DispatcherProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.math.round
 
 //Please note i extend the test view model reason being that its an interface and i can easily
 //implement its members
@@ -27,8 +32,231 @@ class MainViewModel @Inject constructor(
 
     //using state flow
     private val _conversion = MutableStateFlow<CurrencyEvent>(CurrencyEvent.Empty)
-    val conversion : StateFlow<CurrencyEvent> = _conversion
+    var conversion : StateFlow<CurrencyEvent> = _conversion
 
     //create a function that converts the currency and makes the request
+    fun convert(
+        userInputAmountToConvert : String,
+        fromCurrency : String,
+        toCurrency : String
+    ){
+        //convert amount to convert to float and also handle if its null
+        val amountToConvert = userInputAmountToConvert.toFloatOrNull()
+        if(amountToConvert == null){
+            _conversion.value = CurrencyEvent.Failure("Not a valid amount")
+            return
+        }
+
+        viewModelScope.launch(dispatchers.io) {
+            _conversion.value = CurrencyEvent.Loading
+
+            //from currency becomes your base currency when you make the API call
+
+            when(
+                val apiRatesResponse = repository.getDataFromApiInTestRepository(fromCurrency)
+            ){
+                is ApiCallErrorHandler.Error -> _conversion.value = CurrencyEvent.Failure(
+                    apiRatesResponse.message!!
+                )
+
+                is ApiCallErrorHandler.Success -> {
+//                    val rates = apiRatesResponse.data!!.rates
+//
+//                    val ratesConversion = getRatesFromCurrency(toCurrency, rates)
+//
+//                    if(ratesConversion == null){
+//                        _conversion.value = CurrencyEvent.Failure("Unexpected Error")
+//                    }else{
+//                        val convertAmountToConvertTo2DP = round(amountToConvert * ratesConversion * 100) / 100
+//                    }
+                    val rates = apiRatesResponse.data!!.rates
+
+                    val ratesConversion = getRatesFromCurrency(toCurrency, rates).toString()
+
+                    val convert_AmountToConvert_To_2DP = round(
+                        amountToConvert * ratesConversion.toFloat() * 100
+                    ) / 100
+
+                    _conversion.value =
+                        CurrencyEvent.Success(
+                        "$amountToConvert $fromCurrency = $convert_AmountToConvert_To_2DP $toCurrency"
+                    )
+                }
+            }
+
+        }
+    }
+
+    private fun getRatesFromCurrency(currency: String, rates: Rates) = when (currency){
+        "AED" -> rates.AED
+        "AFN" -> rates.AFN
+        "ALL" -> rates.ALL
+        "AMD" -> rates.AMD
+        "ANG" -> rates.ANG
+        "AOA" -> rates.AOA
+        "ARS" -> rates.ARS
+        "AUD" -> rates.AUD
+        "AWG" -> rates.AWG
+        "AZN" -> rates.AZN
+        "BAM" -> rates.BAM
+        "BBD" -> rates.BBD
+        "BDT" -> rates.BDT
+        "BGN" -> rates.BGN
+        "BHD" -> rates.BHD
+        "BIF" -> rates.BIF
+        "BMD" -> rates.BMD
+        "BND" -> rates.BND
+        "BOB" -> rates.BOB
+        "BRL" -> rates.BRL
+        "BSD" -> rates.BSD
+        "BTC" -> rates.BTC
+        "BTN" -> rates.BTN
+        "BWP" -> rates.BWP
+        "BYN" -> rates.BYN
+        "BYR" -> rates.BYR
+        "BZD" -> rates.BZD
+        "CAD" -> rates.CAD
+        "CDF" -> rates.CDF
+        "CHF" -> rates.CHF
+        "CLF" -> rates.CLF
+        "CLP" -> rates.CLP
+        "CNY" -> rates.CNY
+        "COP" -> rates.COP
+        "CRC" -> rates.CRC
+        "CUC" -> rates.CUC
+        "CUP" -> rates.CUP
+        "CVE" -> rates.CVE
+        "CZK" -> rates.CZK
+        "DJF" -> rates.DJF
+        "DKK" -> rates.DKK
+        "DOP" -> rates.DOP
+        "DZD" -> rates.DZD
+        "EGP" -> rates.EGP
+        "ERN" -> rates.ERN
+        "ETB" -> rates.ETB
+        "EUR" -> rates.EUR
+        "FJD" -> rates.FJD
+        "FKP" -> rates.FKP
+        "GBP" -> rates.GBP
+        "GEL" -> rates.GEL
+        "GGP" -> rates.GGP
+        "GHS" -> rates.GHS
+        "GIP" -> rates.GIP
+        "GMD" -> rates.GMD
+        "GNF" -> rates.GNF
+        "GTQ" -> rates.GTQ
+        "GYD" -> rates.GYD
+        "HKD" -> rates.HKD
+        "HNL" -> rates.HNL
+        "HRK" -> rates.HRK
+        "HTG" -> rates.HTG
+        "HUF" -> rates.HUF
+        "IDR" -> rates.IDR
+        "ILS" -> rates.ILS
+        "IMP" -> rates.IMP
+        "INR" -> rates.INR
+        "IQD" -> rates.IQD
+        "IRR" -> rates.IRR
+        "ISK" -> rates.ISK
+        "JEP" -> rates.JEP
+        "JMD" -> rates.JMD
+        "JOD" -> rates.JOD
+        "JPY" -> rates.JPY
+        "KES" -> rates.KES
+        "KGS" -> rates.KGS
+        "KHR" -> rates.KHR
+        "KMF" -> rates.KMF
+        "KPW" -> rates.KPW
+        "KRW" -> rates.KRW
+        "KWD" -> rates.KWD
+        "KYD" -> rates.KYD
+        "KZT" -> rates.KZT
+        "LAK" -> rates.LAK
+        "LBP" -> rates.LBP
+        "LKR" -> rates.LKR
+        "LRD" -> rates.LRD
+        "LSL" -> rates.LSL
+        "LTL" -> rates.LTL
+        "LVL" -> rates.LVL
+        "LYD" -> rates.LYD
+        "MAD" -> rates.MAD
+        "MDL" -> rates.MDL
+        "MGA" -> rates.MGA
+        "MKD" -> rates.MKD
+        "MMK" -> rates.MMK
+        "MNT" -> rates.MNT
+        "MOP" -> rates.MOP
+        "MRO" -> rates.MRO
+        "MUR" -> rates.MUR
+        "MVR" -> rates.MVR
+        "MWK" -> rates.MWK
+        "MXN" -> rates.MXN
+        "MYR" -> rates.MYR
+        "MZN" -> rates.MZN
+        "NAD" -> rates.NAD
+        "NGN" -> rates.NGN
+        "NIO" -> rates.NIO
+        "NOK" -> rates.NOK
+        "NPR" -> rates.NPR
+        "NZD" -> rates.NZD
+        "OMR" -> rates.OMR
+        "PAB" -> rates.PAB
+        "PEN" -> rates.PEN
+        "PGK" -> rates.PGK
+        "PHP" -> rates.PHP
+        "PKR" -> rates.PKR
+        "PLN" -> rates.PLN
+        "PYG" -> rates.PYG
+        "QAR" -> rates.QAR
+        "RON" -> rates.RON
+        "RSD" -> rates.RSD
+        "RUB" -> rates.RUB
+        "RWF" -> rates.RWF
+        "SAR" -> rates.SAR
+        "SBD" -> rates.SBD
+        "SCR" -> rates.SCR
+        "SDG" -> rates.SDG
+        "SEK" -> rates.SEK
+        "SGD" -> rates.SGD
+        "SHP" -> rates.SHP
+        "SLL" -> rates.SLL
+        "SOS" -> rates.SOS
+        "SRD" -> rates.SRD
+        "STD" -> rates.STD
+        "SVC" -> rates.SVC
+        "SYP" -> rates.SYP
+        "SZL" -> rates.SZL
+        "THB" -> rates.THB
+        "TJS" -> rates.TJS
+        "TMT" -> rates.TMT
+        "TND" -> rates.TND
+        "TOP" -> rates.TOP
+        "TRY" -> rates.TRY
+        "TTD" -> rates.TTD
+        "TWD" -> rates.TWD
+        "TZS" -> rates.TZS
+        "UAH" -> rates.UAH
+        "UGX" -> rates.UGX
+        "USD" -> rates.USD
+        "UYU" -> rates.UYU
+        "UZS" -> rates.UZS
+        "VEF" -> rates.VEF
+        "VND" -> rates.VND
+        "VUV" -> rates.VUV
+        "WST" -> rates.WST
+        "XAF" -> rates.XAF
+        "XAG" -> rates.XAG
+        "XAU" -> rates.XAU
+        "XCD" -> rates.XCD
+        "XDR" -> rates.XDR
+        "XOF" -> rates.XOF
+        "XPF" -> rates.XPF
+        "YER" -> rates.YER
+        "ZAR" -> rates.ZAR
+        "ZMK" -> rates.ZMK
+        "ZMW" -> rates.ZMW
+        "ZWL" -> rates.ZWL
+        else -> null
+    }
 
 }
